44c18ecfae05a87db3060de5d3b1fef4d5544561
SebastianPerez0409 <ingsebastian04@gmail.com>
2023-09-13 20:38:32 -0500

    Organize front and back app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:iniciosesion/login.dart';
+import 'package:iniciosesion/Front/login.dart';
 
 // ignore: unused_import
 import 'package:iniciosesion/main.dart';
