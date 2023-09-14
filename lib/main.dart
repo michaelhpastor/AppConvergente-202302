ce33bcabfb2aa8f81c93f6b1eb63967bd337f419
SebastianPerez0409 <ingsebastian04@gmail.com>
2023-09-13 20:43:58 -0500

    Organize UI and Back on App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'UI/login.dart';
 
 void main() {
   runApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:iniciosesion/login.dart';
+import 'package:iniciosesion/UI/login.dart';
 
 // ignore: unused_import
 import 'package:iniciosesion/main.dart';
